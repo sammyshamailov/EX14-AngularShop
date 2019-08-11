655a21e8ac53508b7abc3482d033cd61d7c3bf46
Sammy <sami.shamailov@gmail.com>
2019-08-11 16:06:43 +0300

    removed initial value

--- my-shop/src/app/app.component.ts
+++ my-shop/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class AppComponent {
 
   isVisible: boolean = false;
-  chosenPage: string = "Home";
+  chosenPage: string;
 
   updateVisibility(){
     this.isVisible = !this.isVisible;
