90fab0024c4f272ca3d45d688a90799867f8bb8c
Sammy <sami.shamailov@gmail.com>
2019-08-19 23:26:37 +0300

    data service

---
+++ my-shop/src/app/services/data.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import Product from '../../assets/static/product.json';
+import Category from '../../assets/static/category.json';
+import { IProduct, IProductCategory } from '../../assets/models/index';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+
+  getProducts(): IProduct[]{
+    return Product as IProduct[];
+  }
+
+  getCategories(): IProductCategory[]{
+    return Category as IProductCategory[];
+  }
+
+  getCategoriesName(): string[]{
+    let categoriesName: string[] = [];
+    let categories: IProductCategory[] = this.getCategories();
+    console.log("entered");
+    categoriesName.push("All");
+    for (let i = 0; i < categories.length; i++){
+      let category = categories[i];
+      if(!categoriesName.find(p => p === category.Title)){
+        categoriesName.push(category.Title);
+      }
+    }
+    return categoriesName;
+  }
+}
