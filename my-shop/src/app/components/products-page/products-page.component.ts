6fda46c07b83b44a072110eb69580a840cddf052
Sammy <sami.shamailov@gmail.com>
2019-08-20 02:30:43 +0300

    added cart functionality

--- my-shop/src/app/components/products-page/products-page.component.ts
+++ my-shop/src/app/components/products-page/products-page.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChildren, QueryList, ElementRef, AfterViewInit, AfterViewChecked } from '@angular/core';
 import { IProduct, IProductCategory } from '../../../assets/models/index';
 import { DataService } from 'src/app/services/data.service.js';
+import { CartService, IProductState } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.css'],
-  providers: [DataService]
+  providers: [DataService, CartService]
 })
-export class ProductsPageComponent implements OnInit {
+export class ProductsPageComponent implements OnInit, AfterViewInit, AfterViewChecked {
 
   productsShown: IProduct[];
   @Output() chosenProduct = new EventEmitter<IProduct>();
-
-  constructor(private dataService: DataService) { }
-
+  @ViewChildren ('btn') buttons : QueryList<ElementRef>;
   get productsData(): IProduct[] {return this.dataService.getProducts()};
   get categoryData(): IProductCategory[] {return this.dataService.getCategories()};
   get categories(): string[] {return this.dataService.getCategoriesName()};
+
+  constructor(private dataService: DataService, private cartService: CartService) { }
 
   showChosenCategory(name: string){
     this.productsShown = [];
@@ -34,8 +35,33 @@ export class ProductsPageComponent implements OnInit {
     this.chosenProduct.emit(this.productsData.find(p => p.Title === e.srcElement.alt));
   }
 
+  getProductStatus(title: string): boolean {
+    return this.cartService.getProductState(title);
+  }
+
+  changeProductState(event){
+    let button: ElementRef =  this.buttons.find(button => button.nativeElement.value === event.target.value);
+    let status: boolean = this.getProductStatus(button.nativeElement.value);
+    if(status){
+      this.cartService.removeFromCart(button.nativeElement.value);
+      button.nativeElement.innerHTML = 'add';
+    }
+    else{
+      this.cartService.addToCart(button.nativeElement.value);
+      button.nativeElement.innerHTML = 'remove';
+    }
+  }
+
   ngOnInit() {
     this.productsShown = this.productsData;
+  }
+
+  ngAfterViewInit(){
+    this.buttons.forEach(button => button.nativeElement.innerHTML = this.cartService.getProductState(button.nativeElement.value)? 'remove': 'add');
+  }
+
+  ngAfterViewChecked() {
+    this.buttons.forEach(button => button.nativeElement.innerHTML = this.cartService.getProductState(button.nativeElement.value)? 'remove': 'add');
   }
 
 }
