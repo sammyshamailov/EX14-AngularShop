49d5e0b423b891cef3b909c466b7c566b25f5ac6
Sammy <sami.shamailov@gmail.com>
2019-08-22 16:02:35 +0300

    added carts array

--- my-shop/src/app/services/cart.service.ts
+++ my-shop/src/app/services/cart.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { DataService } from './data.service';
 import { IProduct } from 'src/assets/models';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,22 +9,30 @@ import { IProduct } from 'src/assets/models';
 export class CartService {
 
   private productsInCart: IProduct[] = [];
+  private carts: IProduct[][] = [];
+  private currentCart: IProduct[] = [];
 
-  constructor(private dataService: DataService) {}
+  constructor(
+    private dataService: DataService,
+    private userService: UserService) {
+    for (let i: number = 0; i < this.userService.numOfUsers; i++) {
+      this.carts[i] = new Array<IProduct>();
+    }
+  }
 
-  getProducts(): IProduct[]{
+  getProducts(): IProduct[] {
     return this.productsInCart;
   }
 
-  getProductState(title: string): boolean{
-    return this.productsInCart.find(p => p.Title === title)? true: false;
+  getProductState(title: string): boolean {
+    return this.productsInCart.find(p => p.Title === title) ? true : false;
   }
 
-  addToCart(title: string){
+  addToCart(title: string) {
     this.productsInCart.push(this.dataService.getProducts().find(p => p.Title === title));
   }
 
-  removeFromCart(title: string){
+  removeFromCart(title: string) {
     this.productsInCart.splice(this.productsInCart.findIndex(p => p.Title === title), 1);
   }
 }
