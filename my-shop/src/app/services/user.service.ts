c7811b2b89df9becbb655c971053469aeea7df50
Sammy <sami.shamailov@gmail.com>
2019-08-22 16:03:12 +0300

    added functionality

--- my-shop/src/app/services/user.service.ts
+++ my-shop/src/app/services/user.service.ts
@@ -4,6 +4,7 @@ import Users from '../../assets/static/user.json';
 interface User{
   Username: string;
   Password: string;
+  isAdmin: boolean;
 }
 
 @Injectable({
@@ -11,6 +12,20 @@ interface User{
 })
 export class UserService {
   private users: User[] = Users;
+  private isLogged: boolean = false;
+  private currentUser: User;
+  get numOfUsers(): number {return Users.length};
+  get isLoggedIn(): boolean {return this.isLogged};
+  get isAdmin(): boolean {return this.currentUser.isAdmin};
 
   constructor() { }
+
+  logIn(username: string, password: string): boolean {
+    this.currentUser = this.users.find(p => p.Username === username && p.Password === password);
+    if(this.currentUser){
+      this.isLogged = true;
+      return true;
+    }
+    return false;
+  }
 }
