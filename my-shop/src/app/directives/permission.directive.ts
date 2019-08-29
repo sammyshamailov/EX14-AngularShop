0f27dbd016b8a13dfd1f6f8cbed4ec8ce6505e72
Sammy <sami.shamailov@gmail.com>
2019-08-29 13:01:32 +0300

    added directive functionality

--- my-shop/src/app/directives/permission.directive.ts
+++ my-shop/src/app/directives/permission.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, HostListener, Output, EventEmitter } from '@angular/core';
 
 @Directive({
   selector: '[appPermission]'
 })
 export class PermissionDirective {
+  @Input('appPermission') message: string;
+  @Output() confirmation = new EventEmitter<string>();
 
   constructor() { }
+
+  @HostListener('click') onclick() {
+    this.confirmation.emit(confirm(this.message) ? 'Yes' : 'No');
+  }
 
 }
