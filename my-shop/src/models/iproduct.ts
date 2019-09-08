2c3dc030039b5eb344def2ce6f1aaed42563c3a9
Sammy <sami.shamailov@gmail.com>
2019-09-08 16:24:21 +0300

    initial commit

---
+++ my-shop/src/models/iproduct.ts
@@ -0,0 +1,8 @@
+export interface IProduct {
+    CategoryId: string;
+    Image: string;
+    BigImage: string;
+    Title: string;
+    Price: string;
+    Description: string;
+  }
---
+++ my-shop/src/models/menu-items.ts
@@ -0,0 +1,6 @@
+export enum MenuItems {
+    Home = "Home", Cart = "Cart",
+    Products = "Products", LogIn = "Log In",
+    LogOut = "Log Out", Admin = "Add/Edit",
+    About = "About", Contact = "Contact"
+  }
