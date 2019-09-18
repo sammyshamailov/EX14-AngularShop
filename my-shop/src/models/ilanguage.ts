18dd7d15759737d98ccb282bfb7340f55b9fd08d
Sammy <sami.shamailov@gmail.com>
2019-09-18 20:49:58 +0300

    initial commit

---
+++ my-shop/src/models/ilanguage.ts
@@ -0,0 +1,10 @@
+export interface ILanguage {
+    "Home": string;
+    "Cart": string;
+    "Products": string;
+    "Log In": string;
+    "Log Out": string;
+    "Add/Edit": string;
+    "About": string;
+    "Contact": string;
+}
